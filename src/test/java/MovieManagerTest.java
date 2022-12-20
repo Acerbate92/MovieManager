f42f7c0387cc1683f82e32cda925642572391de7
Alex Razygraev <rav200@mail.ru>
2022-12-20 23:05:26 +0300

    Add test

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -14,4 +14,15 @@ public class MovieManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    public void test2(){
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Harry 1");
+        manager.addMovie("Harry 2");
+        manager.addMovie("Harry 3");
+
+        String[] expected = {"Harry 3", "Harry 2", "Harry 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
