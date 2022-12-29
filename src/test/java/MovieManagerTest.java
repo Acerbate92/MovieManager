a41532b3d9214d680c6f2386ba108f2da6b644f7
Alex Razygraev <rav200@mail.ru>
2022-12-29 19:38:01 +0300

    Add tests

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -14,6 +14,7 @@ public class MovieManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
     public void test2(){
         MovieManager manager = new MovieManager();
         manager.addMovie("Harry 1");
@@ -22,6 +23,47 @@ public class MovieManagerTest {
 
         String[] expected = {"Harry 3", "Harry 2", "Harry 1"};
         String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test3(){
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Harry 1");
+        manager.addMovie("Harry 2");
+        manager.addMovie("Harry 3");
+        manager.addMovie("Harry 4");
+        manager.addMovie("Harry 5");
+        manager.addMovie("Harry 6");
+        manager.addMovie("Harry 7");
+        manager.addMovie("Harry 8");
+        manager.addMovie("Harry 9");
+        manager.addMovie("Harry 10");
+
+        String[] expected = {"Harry 1","Harry 2","Harry 3","Harry 4","Harry 5","Harry 6","Harry 7","Harry 8", "Harry 9", "Harry 10"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test4(){
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Harry 1");
+        manager.addMovie("Harry 2");
+        manager.addMovie("Harry 3");
+        manager.addMovie("Harry 4");
+        manager.addMovie("Harry 5");
+        manager.addMovie("Harry 6");
+        manager.addMovie("Harry 7");
+        manager.addMovie("Harry 8");
+        manager.addMovie("Harry 9");
+        manager.addMovie("Harry 10");
+        manager.addMovie("Harry 11");
+
+        String[] expected = {"Harry 1","Harry 2","Harry 3","Harry 4","Harry 5","Harry 6","Harry 7","Harry 8", "Harry 9", "Harry 10", "Harry 11"};
+        String[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
